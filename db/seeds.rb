192a05ef7e997d5498b8039bb45fc347ce289f8e
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-05-23 16:51:35 +0200

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require_relative "../app/models/article"
+require 'active_record'
+
+puts 'Creating posts...'
+10.times do
+  article = Article.new(title: Faker::TvShows::RuPaul.queen, content: Faker::Movies::LordOfTheRings.location)
+  article.save!
+end
+
+puts 'Finished!'
